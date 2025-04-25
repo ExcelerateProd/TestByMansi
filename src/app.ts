e7823a140e71c0859a65ece7afd86e3bdf62bcd2
Yash-VEmpower <107692752+Yash-VEmpower@users.noreply.github.com>
2025-04-25 17:03:51 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 8087;
+
+app.get('/', (req, res) => {
+  res.send('Launching Soon..........................!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
